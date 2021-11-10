af1b8224ce74635825114e678943ebff0d652e09
Srijan Paul <srijan.paul@deepsource.io>
2021-11-10 14:36:21 +0530

    e.e :

--- packages/demo-typescript/src/index.ts
+++ packages/demo-typescript/src/index.ts
@@ -1,27 +1,8 @@
-class SampleClass {
-    files = {}
-    a: number
-    b: number
-
-    public static get myField1() {
-      return 1;
-    }
-
-    public constructor() {
-      
-    }
-  
-    private get ['myField2']() {
-        const x = { ...this.files } as T;
-        if (false) {
-            // @ts-ignore: Unreachable code error
-            return 'hello world';
-          } if(x){
-              return x
-          }else {
-              return 'hello world'
-          }
-      
-    }
+class C {
+  a: number
+  b: number
+  constructor() {
+    this.a = 1,
+    this.b = 2
   }
-  
+}
