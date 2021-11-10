b73f589fe7e6ea0b916b4bf6974a935c42a6516a
Srijan Paul <srijan.paul@deepsource.io>
2021-11-10 16:00:40 +0530

    ...

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 import { format } from 'path'
 import * as fs from 'fs' 
 
-class Stuff {
-    constructor() {
-       this.x = 1,
-       this.y = 2,
-       this.z = 3 
-    }
-}
-
-const x = new Stuff()
-export default x
+1
