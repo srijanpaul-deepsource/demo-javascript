3a887026f68c73f4510de3855fc2e378ba2995d7
Srijan Paul <srijan.paul@deepsource.io>
2022-02-09 19:09:20 +0530

    ADADADAD

--- index.js
+++ index.js
@@ -1,73 +1,11 @@
-import { format } from 'path'
-import * as fs from 'fs' 
+let x = 1
+let b = 2
 
-class S {}
-
-new S()
-
-function f() {
+function foo() {
     "use strict";
-    return 1
 }
 
-f()
+console.log(foo())
 
-
-let x = 1;
-x = 2;
-x = 3;
-
-
-let y = []
-y[1] = 2;
-y[1] = 3;
-console.log("hey")
-
-y[2] = 3
-y[2] = 2
-
-if (y.length < 0) {
-    //stuff
-}
-
-class Foo {
-    bar() { return 123 }
-    foo = () => this.bar()
-}
-
-const f = new Foo()
-f.foo()
-
-const foo = "foo"
-const baz = 'baz'
-var f = () => 'foo'
-var g = () => f
-var h = () => g
-var i = () => h 
-var j = () => i
-var k = () => j
-var l = () => k
-var m = () => l
-var n = () => m
-
-console.log("HELLO!");
-(() => {
-    "use strict";
-    return 1;
-});
-
-let x = 100;
-let y = 20;
-
-let y = (x) => {
-    if (x) {
-        return 1;
-    }
-}
-
-let z = y
-return foo
-let x = () => 1
-let z = () => 2
-
-"use foo";
+(() => 1)
+(() => 2)
