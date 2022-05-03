22b1e15d61c8a4eb832e01b2dbcaca4f51d1bf01
Srijan Paul <srijan.paul@deepsource.io>
2022-05-03 06:28:36 +0530

    foo

--- esm.ts
+++ esm.ts
@@ -1,14 +1,5 @@
 let x: any = 1
 
-if (x == null && typeof x == 'number') {
-  x = 2
-}
-
-const f = <T>(x: T) => x
-f(x == null)
-f((x == null) || false)
-f(x == null || false)
-export const c = (x != null) && (x == null) && (x != null)
 x = x == null == null == undefined == false 
 
 export default x
