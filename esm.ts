c3d46c7567e74e617c783b1334f860f2d37c1a44
Srijan Paul <srijan.paul@deepsource.io>
2022-05-03 06:25:32 +0530

    foo

--- esm.ts
+++ esm.ts
@@ -4,4 +4,10 @@ if (x == null && typeof x == 'number') {
   x = 2
 }
 
+const f = <T>(x: T) => x
+f(x == null)
+f((x == null) || false)
+f(x == null || false)
+export const c = (x != null) && (x == null) && (x != null)
+
 export default x
