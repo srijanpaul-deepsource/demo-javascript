aad5510dfee9efa322f9ee0cd5866d690d23d02e
Srijan Paul <srijan.paul@deepsource.io>
2022-07-07 14:07:34 +0530

    foo

--- packages/demo-react/src/App.js
+++ packages/demo-react/src/App.js
@@ -16,7 +16,17 @@ function App() {
 
 class YourComponent extends React.Component {
   render() {
+    let x = <Foo a a />
+    let iframe = <iframe src="" frameborder="0"></iframe>
+
     return <>hello</>
+  }
+}
+
+class Foo extends Component {
+  getSnapshotBeforeUpdate(prevProps, nextProps){}
+  render() {
+    return <div> single quote: '</div>
   }
 }
 
