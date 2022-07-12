62bfd2d7cec37f6cad27cbaa1709d8986d0cdf85
Srijan Paul <srijan.paul@deepsource.io>
2022-07-12 12:23:47 +0530

    foo

--- esm.ts
+++ esm.ts
@@ -2,9 +2,12 @@ export const p = new Promise((resolve, _reject) => {
   resolve(100);
 })
 
-let x = 1
+export const p2 = new Promise((resolve) => resolve(200))
+
+let x: number | boolean = 1
 x =+ 1
 x =- 2
+x =! 3
 x =! 3
 
 export default x
