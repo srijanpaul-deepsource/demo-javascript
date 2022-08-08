69a43edf28ecfe419d36975ae59aed50660b6ea3
Srijan Paul <srijan.paul@deepsource.io>
2022-08-08 09:40:05 +0530

    foo

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const mysymbols = new Symbol(new Symbol([new Symbol()], (1, 2, 3, new Symbol()))
 const o = { x: 1 }
 const p = { y: 2 }
 
+// Commnet comment comment
 // JS-0084
 o.__proto__ = p
 console.log(o.__proto__, p.__proto__)
 console.log(Object.getPrototypeOf(o.__proto__))
 console.log(Object.setPrototypeOf(p.__proto__, p))
 o["__proto__"] = p["__proto__"]
+o["__proto__"] = p["__proto__"].__proto__
 o.__proto__ = p.__proto__
