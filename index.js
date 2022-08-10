0f2ef360c0b242ea4ca1543d95ab76f86f2cab9b
Srijan Paul <srijan.paul@deepsource.io>
2022-08-10 14:49:03 +0530

    stuff

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const p = { y: 2 }
 // Commnet comment comment
 // JS-0084
 o.__proto__ = p
-console.log(o.__proto__, p.__proto__)
-console.log(Object.getPrototypeOf(o.__proto__))
-console.log(Object.setPrototypeOf(p.__proto__, p))
 o["__proto__"] = p["__proto__"]
 o["__proto__"] = p["__proto__"].__proto__
 o.__proto__ = p.__proto__
+
+console.log(o.__proto__, p.__proto__)
+console.log(Object.getPrototypeOf(o.__proto__))
+console.log(Object.setPrototypeOf(p.__proto__, p))
 
 export default 123
