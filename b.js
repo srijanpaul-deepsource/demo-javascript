6a6da2726ac7fe107c919cbd5415f27a54d086c8
Srijan Paul <srijan.paul@deepsource.io>
2022-08-22 13:01:49 +0530

    foo

--- b.js
+++ b.js
@@ -1,1 +1,33 @@
 import a from './a'
+
+const cp = require("child_process")
+const { exec } = require("child_process")
+
+const proc = "foobar"
+cp.exec(proc) // unsafe
+
+cp.exec("ls") // safe
+
+cp.execFile(proc) // unsafe
+
+exec("fubar") // safe
+exec("./" + proc)  // unsafe
+
+export async function f(x) {
+  return async function g() {
+    return async function h() {
+      return x
+    }
+  }
+}
+
+export async function f2(xs) {
+  const ys = []
+  for await (const x of xs) {
+    ys.push(x)
+  }
+  return ys
+}
+
+
+export default 2
