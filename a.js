12daa73ffa5c0407ced0e3683d5b82b0539662d5
Srijan Paul <srijan.paul@deepsource.io>
2022-08-22 12:30:33 +0530

    foo

--- a.js
+++ a.js
@@ -3,4 +3,32 @@ import b from './b'
 let foo = {}
 let bar = foo?.foobar
 
+const cp = require("child_process")
+const { exec } = require("child_process")
+
+const proc = "foobar"
+cp.exec(proc) // unsafe
+
+cp.exec("ls") // safe
+
+cp.execFile(proc) // unsafe
+
+exec("fubar") // safe
+exec("./" + proc)  // unsafe
+
+export async function f() {
+  return async function g() {
+    return 1
+  }
+}
+
+export async function f2(xs) {
+  const ys = []
+  for await (const x of xs) {
+    ys.push(x)
+  }
+  return ys
+}
+
+
 export default 2
--- server.js
+++ server.js
@@ -8,30 +8,3 @@ const app = require('express')()
 app.get('/foo', (req, res) => {
   vm.run(req.params.foo);
 });
-
-const cp = require("child_process")
-const { exec } = require("child_process")
-
-const proc = "foobar"
-cp.exec(proc) // unsafe
-
-cp.exec("ls") // safe
-
-cp.execFile(proc) // unsafe
-
-exec("fubar") // safe
-exec("./" + proc)  // unsafe
-
-export async function f() {
-  return async function g() {
-    return 1
-  }
-}
-
-export async function f2(xs) {
-  const ys = []
-  for await (const x of xs) {
-    ys.push(x)
-  }
-  return ys
-}
