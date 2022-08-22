04ef1e53ebd932d85b2defa00ddd8798080b6e3c
Srijan Paul <srijan.paul@deepsource.io>
2022-08-22 13:29:24 +0530

    foo

--- a.js
+++ a.js
@@ -3,7 +3,8 @@ import b from './b'
 let foo = {}
 let bar = foo?.foobar
 
-import cp from 'child_process'
+
+import cp from "child_process"
+cp.exec('foo' + bar)
 const xs = "..." + "something"
 cp.cxecFile(xs)
-
