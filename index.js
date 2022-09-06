034e127ca5bc54fb7dcd2f07dedd252fb3ed6250
Srijan Paul <srijan.paul@deepsource.io>
2022-09-06 16:37:12 +0530

    foo

--- index.js
+++ index.js
@@ -38,31 +38,26 @@ cp.cxecFile(xs)
 exec('foo' + bar)
 execFile('foo' + bar)
 
-const helmet = require('helmet')
-helmet({
-  contentSecurityPolicy: {
-    directives: {
-      frameAncestors: 'none'
-    }
-  }
-})
-
-helmet({ contentSecurityPolicy: false });
-helmet.contentSecurityPolicy({
-  directives: {
-    frameAncestors: 'none'
-  }
-});
-
-const xml = require('libxmljs');
-xml.parseXmlString('foo', {
-  noent: true
-});
-
-xml.parseXmlString('foo', {
-  noent: false
-});
+import helmet, { hsts } from 'helmet'
+import exp, { static as stat } from 'express'
+import lib from 'libxmljs';
 
 const app = exp()
-app.use(hsts({ includeSubdomains: false }))
+app.use(hsts({ includeSubdomains: false })) // 1002 - 
 
+app.use(helmet.dnsPrefetchControl({ allow: true })) // 1003
+
+express.use(stat('public', { dotfiles: 'allow' })) // d020 -
+
+const h = require('helmet'); h({ noSniff: false }) // 1001 -
+
+lib.parseXmlString('', { noent: true }) // d022
+
+Buffer([1, 2, 3]) // d026
+new Buffer([1, 2, 3])
+new Buffer(10)
+
+helmet.contentSecurityPolicy({ directives: { frameAncestors: 'none' } }) // d024
+const { contentSecurityPolicy } = require('helmet')
+const foo = 1
+contentSecurityPolicy({ directives: { frameAncestors: 'none', foo } })
