e18c066cb9a5359b7988e09f434ca44089417fd0
Srijan Paul <srijan.paul@deepsource.io>
2022-10-07 13:28:14 +0530

    foo

--- esm.ts
+++ esm.ts
@@ -1,19 +1,21 @@
+import f from './foo'
+
 export const p = new Promise((resolve, _reject) => {
   resolve(100);
-  })
+});
 
-export const p2 = new Promise((resolve) => resolve(200))
+export const p2 = new Promise((resolve) => resolve(200));
 
-let x: number | boolean = 1
-  x =+ 1
-x =- 2
-  x =! 3
-x =! 3
+let x: number | boolean = 1;
+x = +1;
+x = -2;
+x = !3;
+x = !3;
 
-  export default x
+export default x;
 
 export class C {
   private async f(x: boolean): Promise<boolean> {
-    return !!!x
+    return !!!x;
   }
 }
