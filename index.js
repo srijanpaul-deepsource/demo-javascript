2a7e681370bf3018f43a63ef0330318deb9f5f75
Srijan Paul <srijan.paul@deepsource.io>
2022-12-13 15:52:42 +0530

    foo

--- index.js
+++ index.js
@@ -1,76 +1,73 @@
-import cp from "child_process"
-import { execFile } from 'child_process'
-import helmet, { hsts } from 'helmet'
-import exp, { static as stat } from 'express'
-import lib from 'libxmljs';
+import cp from "child_process";
+import { execFile } from "child_process";
+import helmet, { hsts } from "helmet";
+import exp, { static as stat } from "express";
+import lib from "libxmljs";
 
 // JS-0233
-const symbol = new Symbol()
-const mysymbols = new Symbol(new Symbol([new Symbol()], (1, 2, 3, new Symbol())));
+const symbol = new Symbol();
+const mysymbols = new Symbol(
+  new Symbol([new Symbol()], (1, 2, 3, new Symbol()))
+);
 
-const o = { x: 1 }
-const p = { y: 2 }
+const o = { x: 1 };
+const p = { y: 2 };
 
 // Commnet comment comment
 // JS-0084
-o.__proto__ = p
-o["__proto__"] = p["__proto__"]
-o["__proto__"] = p["__proto__"].__proto__
-o.__proto__ = p.__proto__
+o.__proto__ = p;
+o["__proto__"] = p["__proto__"];
+o["__proto__"] = p["__proto__"].__proto__;
+o.__proto__ = p.__proto__;
 
-console.log(o.__proto__, p.__proto__)
-console.log(Object.getPrototypeOf(o.__proto__))
-console.log(Object.setPrototypeOf(p.__proto__, p))
+console.log(o.__proto__, p.__proto__);
+console.log(Object.getPrototypeOf(o.__proto__));
+console.log(Object.setPrototypeOf(p.__proto__, p));
 
 class F {
-  constructor() {
-  }
+  constructor() {}
 }
 
-new F()
+new F();
 
-const { exec } = require('cp')
+const { exec } = require("cp");
 
-const bar = 'bar'
-cp.exec('foo' + bar)
-const xs = "..." + "something"
-cp.cxecFile(xs)
-exec('foo' + bar)
-execFile('foo' + bar)
+const bar = "bar";
+cp.exec("foo" + bar);
+const xs = "..." + "something";
+cp.cxecFile(xs);
+exec("foo" + bar);
+execFile("foo" + bar);
 
+const app = exp();
+app.use(hsts({ includeSubdomains: false })); // 1002 -
 
-const app = exp()
-app.use(hsts({ includeSubdomains: false })) // 1002 - 
+const app2 = exp();
+app2.use(hsts({ includeSubdomains: false })); // 1002 -
 
+app.use(helmet.dnsPrefetchControl({ allow: true })); // 1003
 
-const app2 = exp()
-app2.use(hsts({ includeSubdomains: false })) // 1002 - 
+express.use(stat("public", { dotfiles: "allow" })); // d020 -
 
-app.use(helmet.dnsPrefetchControl({ allow: true })) // 1003
+const h = require("helmet");
+h({ noSniff: false }); // 1001 -
 
-express.use(stat('public', { dotfiles: 'allow' })) // d020 -
+lib.parseXmlString("", { noent: true }); // d022
 
-const h = require('helmet'); h({ noSniff: false }) // 1001 -
+Buffer([1, 2, 3]); // d026
+new Buffer([1, 2, 3]);
+new Buffer(10);
 
-lib.parseXmlString('', { noent: true }) // d022
+helmet.contentSecurityPolicy({ directives: { frameAncestors: "none" } }); // d024
+const { contentSecurityPolicy } = require("helmet");
+const foo = 1;
+contentSecurityPolicy({ directives: { frameAncestors: "none", foo } });
 
-Buffer([1, 2, 3]) // d026
-new Buffer([1, 2, 3])
-new Buffer(10)
+const div = document.getElementById("foo");
 
-helmet.contentSecurityPolicy({ directives: { frameAncestors: 'none' } }) // d024
-const { contentSecurityPolicy } = require('helmet')
-const foo = 1
-contentSecurityPolicy({ directives: { frameAncestors: 'none', foo } })
+fetch("foo").then((res) => (div.innerHTML = res.data.foo));
 
-const div = document.getElementById('foo')
-
-fetch('foo', (res) => {
-  div.innerHTML = res.data.foo
-})
-
-window.postMessage("secret message for the parent")
+window.postMessage("secret message for the parent");
 
 if (false) {
 }
-
