b605bcc843a731448a1c91e721536aba2dae734e
Srijan Paul <srijan.paul@deepsource.io>
2023-01-10 10:18:16 +0530

    foo

--- new.js
+++ new.js
@@ -5,4 +5,9 @@ const buf = new Buffer("fubar")
 const r = new RegExp('[a-Z][A-z0-9]')
 const r2 = /[A-zF-a0-9]+/g
 
+const x = 1
+function add(x, y) {
+    return x + y
+}
+
 export { x, buf, r, r2 }
