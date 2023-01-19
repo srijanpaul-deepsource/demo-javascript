4bb7e5aa6765c72f970b81e8fb37aeab6a810922
Srijan Paul <srijan.paul@deepsource.io>
2023-01-19 22:01:55 +0530

    foo

--- new.js
+++ new.js
@@ -1,16 +1,6 @@
-import x from "./does-not-exist";
-
-const buf = new Buffer("fubar");
-
-const r = new RegExp("[a-Z][A-z0-9]");
-const r2 = /[A-zF-a0-9]+/g;
-
-const object = { a: 1, b: 2 };
+const object = { a: 1, b: 2, c: 3, d: 4 };
 let { a, b } = object;
-a += 12;
+let { c, d } = object;
+a += 1;
 
-const anotherObject = { c: 2, d: 4 };
-let { c, d } = anotherObject; // JS-0242
-
-x(b)
-export { x, buf, r, r2, a, b, c };
+export default { a, b, c, d };
